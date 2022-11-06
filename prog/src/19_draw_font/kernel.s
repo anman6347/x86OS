3717d392cd71858469ef3dff1eed5bf01845f2b4
anman6347 <4620096@ed.tus.ac.jp>
2022-11-07 00:40:31 +0900

    :sparkles: add: フォント一覧表示関数の実装

---
+++ prog/src/19_draw_font/kernel.s
@@ -0,0 +1,36 @@
+    ; --- macro ---
+    %include    "../include/define.s"
+    %include    "../include/macro.s"
+
+    ORG     KERNEL_LOAD
+
+[BITS 32]
+
+    ; --- entry point ---
+kernel:
+    ; get 8 * 16 BIOS font address (exsisted at the end of MBR)
+    mov     esi, BOOT_LOAD + SECT_SIZE      ; ESI = 0x7c00 + 512
+    movzx   eax, word [esi + 0]             ; segment for font addr (ESI + 0 not equals DS:ESI)
+    movzx   ebx, word [esi + 2]             ; offset for font addr
+    shl     eax , 4
+    add     eax, ebx                        ; seg:off in real mode
+    mov     [FONT_ADR], eax
+
+    ; display all characters obtained by int 0x10 order
+    cdecl	draw_font, 63, 13
+
+
+    ; while(1)
+    jmp     $
+
+
+ALIGN 4, db 0
+FONT_ADR:   dd  0
+
+; --- modules ---
+    %include    "../modules/protect/vga.s"
+    %include    "../modules/protect/draw_char.s"
+    %include    "../modules/protect/draw_font.s"
+
+; --- padding ---
+    times KERNEL_SIZE - ($ - $$) db 0x00
---
+++ prog/src/modules/protect/draw_font.s
@@ -0,0 +1,56 @@
+; void draw_font(int32 col, int32 row);
+
+; display all characters obtained by int 0x10 order
+
+; col: column where the character is displayed. this parm must take a value between 0 and 79.
+; row: row where the character is displayed. this parm must take a value between 0 and 29.
+; col and row is not XY dot of screen
+
+draw_font:
+    ; build stack frame
+    push    ebp
+    mov     ebp, esp
+
+    ; save registers' data
+    push    eax
+    push    ebx
+    push    ecx
+    push    edx
+    push    esi
+    push    edi
+
+    ; get args
+    mov     esi, [ebp + 8]      ; ESI = x(col)
+    mov     edi, [ebp + 12]     ; EDI = y(row)
+
+    ; display all characters obtained by int 0x10 order
+    mov     ecx, 0
+.10L:
+    cmp     ecx, 256                            ; while (ECX < 256) {
+    jae     .10E 
+
+    ; calc x(col)
+    mov     eax, ecx                            ;   EAX = ECX;
+    and     eax, 0x0F                           ;   EAX &= 0x0F;
+    add     eax, esi                            ;   EAX += x;
+
+    mov     ebx, ecx                            ;   EBX = ECX;
+    shr     ebx, 4                              ;   EBX /= 16;
+    add     ebx, edi                            ;   EBX += y;
+
+    cdecl   draw_char, eax, ebx, 0x07, ecx      ;   draw_char();
+
+    inc     ecx                                 ;   ECX++;
+    jmp     .10L                                ; }
+.10E:
+
+    ; return registers' data
+    pop     edx
+    pop     eax
+
+    ; destroy stack frame
+    mov     esp, ebp
+    pop     ebp
+
+    ret
+
